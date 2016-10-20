5518a9b2f02b47a1cd90dc305bbaf39c03e55212
pcolonna <pierre.col.di@gmail.com>
2016-10-20 14:08:51 -0400

    Finishing fixing presentation/indentation...

--- Chapter_1/checkPermutation.py
+++ Chapter_1/checkPermutation.py
@@ -9,22 +9,22 @@ import numpy as np
 
 def check_permutation1(string1, string2):
 
-	if len(string1) != len(string2):
-		return False					# String of diff length can't be permutation...
+    if len(string1) != len(string2):
+        return False                    # String of diff length can't be permutation...
 
-	char_set = [0 for _ in range(128)]	# Array the length of the ASCII table.
+    char_set = [0 for _ in range(128)]  # Array the length of the ASCII table.
 
-	for char in string1:
-		char_set[ord(char)] += 1
-	for char in string2:
-		char_set[ord(char)] -= 1
+    for char in string1:
+        char_set[ord(char)] += 1
+    for char in string2:
+        char_set[ord(char)] -= 1
 
-	# check every value in char_set
-	for element in char_set:
-		if element != 0:				# If not zero, the character appeared a diff number of times in one string. 
-			return False
+    # check every value in char_set
+    for element in char_set:
+        if element != 0:                # If not zero, the character appeared a diff number of times in one string. 
+            return False
 
-	return True
+    return True
 
 # Second version with numpy.
 # 
@@ -35,44 +35,44 @@ def check_permutation1(string1, string2):
 
 def check_permutation(string1, string2):
 
-	if len(string1) != len(string2):
-		return False					# String of diff length can't be permutation...
+    if len(string1) != len(string2):
+        return False                    # String of diff length can't be permutation...
 
-	char_set = [0 for _ in range(128)]	# Array the length of the ASCII table.
+    char_set = [0 for _ in range(128)]  # Array the length of the ASCII table.
 
-	for char in string1:
-		char_set[ord(char)] += 1
-	for char in string2:
-		char_set[ord(char)] -= 1
+    for char in string1:
+        char_set[ord(char)] += 1
+    for char in string2:
+        char_set[ord(char)] -= 1
 
-	
-	return not np.any(char_set)			
-	
+    
+    return not np.any(char_set)         
+    
 # Test from solutions.
 # In original code, pass test_string instead of test_string[0],test_string[1].
 class Test(unittest.TestCase):
-	dataT = [(['abcd', 'bacd']), (['3563476', '7334566']),
-			 (['wef34f', 'wffe34'])]
-	dataF = [(['abcd', 'd2cba']), (['2354', '1234']), (['dcw4f', 'dcw5f'])]
+    dataT = [(['abcd', 'bacd']), (['3563476', '7334566']),
+             (['wef34f', 'wffe34'])]
+    dataF = [(['abcd', 'd2cba']), (['2354', '1234']), (['dcw4f', 'dcw5f'])]
 
-	def test_cp(self):
-		
-		# true check
-		for test_string in self.dataT:
-			actual = check_permutation(test_string[0],test_string[1])
-			self.assertTrue(actual)
-		
-		# false check
-		for test_string in self.dataF:
-			actual = check_permutation(test_string[0],test_string[1])
-			self.assertFalse(actual)
+    def test_cp(self):
+        
+        # true check
+        for test_string in self.dataT:
+            actual = check_permutation(test_string[0],test_string[1])
+            self.assertTrue(actual)
+        
+        # false check
+        for test_string in self.dataF:
+            actual = check_permutation(test_string[0],test_string[1])
+            self.assertFalse(actual)
 
 
 if __name__ == "__main__":
 
-	str1 = "asa"
-	str2 = "aaa"
-	
-	#check_permutation(str1, str2)
-	unittest.main()
-	
+    str1 = "asa"
+    str2 = "aaa"
+    
+    #check_permutation(str1, str2)
+    unittest.main()
+    
--- Chapter_1/isUnique.py
+++ Chapter_1/isUnique.py
@@ -1,5 +1,4 @@
-
-#	Question 1: isUnique
+# Question 1: isUnique
 
 import unittest
 import random
@@ -11,16 +10,16 @@ import string
 # Time complexity O(n^2).
 
 def isUnique(yourString):
-	
-	if len(yourString) > 128:
-		return False
+    
+    if len(yourString) > 128:
+        return False
 
-	else:
-		for i in range(len(yourString)):
-			for j in range(len(yourString)):
-				if i != j and yourString[i] == yourString[j]:
-					return False
-		return True
+    else:
+        for i in range(len(yourString)):
+            for j in range(len(yourString)):
+                if i != j and yourString[i] == yourString[j]:
+                    return False
+        return True
 
 
 
@@ -32,68 +31,68 @@ def isUnique(yourString):
 # Time complexity O(n).
 
 def isUnique2(string):
-	# Assuming character set is ASCII (128 characters)
-	if len(string) > 128:
-		return False
+    # Assuming character set is ASCII (128 characters)
+    if len(string) > 128:
+        return False
 
-	char_set = [False for _ in range(128)]						# Fill an array of boolean, set to False. For now, no character has been encountered.
-	for char in string:
-		val = ord(char)											# ord() expected string of length 1. Returm a number e.g ord("a") = 97, ord("3") = 51
-		if char_set[val]:										# used as index in the char_set
-			# Char already found in string
-			return False
-		char_set[val] = True
+    char_set = [False for _ in range(128)]                      # Fill an array of boolean, set to False. For now, no character has been encountered.
+    for char in string:
+        val = ord(char)                                         # ord() expected string of length 1. Returm a number e.g ord("a") = 97, ord("3") = 51
+        if char_set[val]:                                       # used as index in the char_set
+            # Char already found in string
+            return False
+        char_set[val] = True
 
-	return True
+    return True
 
 
 # Class testing our 2 functions.
 # From github book solutions + some added tests.
 class Test(unittest.TestCase):
-	
-	dataT = [('abcd'), ('s4fad'), ('')]
-	dataF = [('23ds2'), ('hb 627jh=j ()')]
+    
+    dataT = [('abcd'), ('s4fad'), ('')]
+    dataF = [('23ds2'), ('hb 627jh=j ()')]
 
-	# Random string longer than the number of ASCII character. 
-	rdm_string = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(129))
-	print(rdm_string)
+    # Random string longer than the number of ASCII character. 
+    rdm_string = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(129))
+    print(rdm_string)
 
-	def test_unique(self):
-		
-		# true check
-		for test_string in self.dataT:
-			actual  = isUnique(test_string)
-			actual2 = isUnique2(test_string)
-			self.assertTrue(actual)
-			self.assertTrue(actual2)
-		
-		# false check
-		for test_string in self.dataF:
-			actual = isUnique(test_string)
-			actual2 = isUnique2(test_string)
-			self.assertFalse(actual)
-			self.assertFalse(actual2)
+    def test_unique(self):
+        
+        # true check
+        for test_string in self.dataT:
+            actual  = isUnique(test_string)
+            actual2 = isUnique2(test_string)
+            self.assertTrue(actual)
+            self.assertTrue(actual2)
+        
+        # false check
+        for test_string in self.dataF:
+            actual = isUnique(test_string)
+            actual2 = isUnique2(test_string)
+            self.assertFalse(actual)
+            self.assertFalse(actual2)
 
-		# Length check.
-		actual  = isUnique(rdm_string)
-		actual2 = isUnique2(rdm_string)
-		self.assertFalse(actual)
-		self.assertFalse(actual2)
+        # Length check.
+        actual  = isUnique(rdm_string)
+        actual2 = isUnique2(rdm_string)
+        self.assertFalse(actual)
+        self.assertFalse(actual2)
 
-			
+            
 
 
 
 if __name__ == "__main__":
-	
-	#isUnique('abc')
-	
-	rdm_string = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(12))
-	print(rdm_string)
-	
-	unittest.main()
+    
+    #isUnique('abc')
+    
+    rdm_string = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(12))
+    print(rdm_string)
+    
+    unittest.main()
 
-	# Anything after unittest is not executed. 
-	#rdm_string = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(12))
-	#print(rdm_string)
-	
+    # Anything after unittest is not executed. 
+    #rdm_string = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(12))
+    #print(rdm_string)
+    
