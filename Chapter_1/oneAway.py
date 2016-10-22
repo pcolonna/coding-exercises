73b1229bfc66e2fce5efce7b5381ce921b3b210d
pcolonna <pierre.col.di@gmail.com>
2016-10-21 20:31:32 -0400

    Cleaning up. Done

--- Chapter_1/oneAway.py
+++ Chapter_1/oneAway.py
@@ -1,15 +1,17 @@
 # Question 5: One Away.
 
-import unittest
+"""
+        We want to know if two strings are one edit away from one another.
+        Three types of edits are possibles: insert, remove or replace a character.
+        
+        Let's reason about the length of the strings.
+        
+        If the diff >= 2 -> we can't get one string from the other with only one edit (min 2 insertions/deletions.)
+        If dif == 1      -> we can check if one insertion or remove is enough.
+        If dif == 0      -> the only possible edit is to replace a character.
+"""
 
-# We want to know if two strings are one edit away from one another.
-# Three types of edits are possibles: insert, remove or replace a character.
-# 
-# Let's reason about the length of the strings.
-# 
-# If the diff >= 2 -> we can't get one string from the other with only one edit (min 2 insertions/deletions.)
-# If dif == 1      -> we can check if one insertion or remove is enough.
-# If dif == 0      -> the only possible edit is to replace a character.
+import unittest
 
 def one_away(string1, string2):
 
@@ -23,32 +25,33 @@ def one_away(string1, string2):
         return check_replace(string1, string2)
 
 
-# Here we performs the operations to check if only one insertion is necessary.
-# 
-# We compare character by character. 
-# 
-# If they're equals, great. Otherwise we check the variable already_inserted 
-# to see if already performs the edit.
-# 
-# If already_inserted == True,  another edit is therefore necessay and we fail.
-# If already_inserted == False, we set it to True and change the index of the 
-# character we look at in the other string.
-# 
-# example: aba    string 1
-#          ba     string 2
-#          
-#          -> we look first at string1[0] and string2[0]. 
-#          -> then we look at string1[1] but at string2[0] again.
-# 
-# I chose to look at the longer first. So we need to check the length first.
-
+"""
+        Here we performs the operations to check if only one insertion is necessary.
+        
+        We compare character by character. 
+        
+        If they're equals, great. Otherwise we check the variable already_inserted 
+        to see if already performs the edit.
+        
+        If already_inserted == True,  another edit is therefore necessay and we fail.
+        If already_inserted == False, we set it to True and change the index of the 
+        character we look at in the other string.
+        
+        example: aba    string 1
+                 ba     string 2
+                 
+                 -> we look first at string1[0] and string2[0]. 
+                 -> then we look at string1[1] but at string2[0] again.
+        
+        I chose to look at the longer first. So we need to check the length first.
+"""
 def check_insertion(string1, string2):
 
     already_inserted = False
     i, i_short_str = 0, 0
-
+                                                                            # Actually this assignement is not usefulll anymore...
     long_str  = string1 if len(string1) > len(string2) else string2         # Conditional assignement, depending on the length of each string.
-    short_str = string1 if len(string1) < len(string2) else string2         # Probaly better than if...else, but could be improved.
+    short_str = string1 if len(string1) < len(string2) else string2         # Probaly better than if-else, but could be improved.
 
     while i < len(long_str) and i_short_str < len(short_str):
        
@@ -66,9 +69,11 @@ def check_insertion(string1, string2):
     return True
 
 
-# If the same length, we check how many replacement we need to make.
-# We just compare character by character.
 
+"""
+        If the same length, we check how many replacement we need to make.
+        We just compare character by character.
+"""
 def check_replace(string1, string2):
 
     already_replaced = False
