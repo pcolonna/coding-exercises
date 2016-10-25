eccaab86d40d012c15fe9ee56802c03624c4c1c6
pcolonna <pierre.col.di@gmail.com>
2016-10-25 11:56:02 -0400

    String rotation initial commit

---
+++ Chapter_1/1.9_stringRotation.py
@@ -0,0 +1,9 @@
+# Question 1.9: String rotation
+
+"""
+We have two strings, s1 and s2. We want to know if s2 is a rotation of s1 using only one call to 
+is_substring.
+
+is_substring checks if one word is a substring of another.
+"""
+
