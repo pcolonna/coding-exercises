50522d04e22fa213b7d9792569ac24b6dc769c13
pcolonna <pierre.col.di@gmail.com>
2016-10-28 16:34:37 -0400

    First version done.

--- Chapter_2/2.1_removeDups.py
+++ Chapter_2/2.1_removeDups.py
@@ -2,3 +2,28 @@
 
 """ Remove duplicates from an unsorted linked list. """
 
+import unittest
+
+"""
+	We go through the list element by element. 
+	If the element is not already in our cleaned up list, we append it to it.
+	Otherwise, we move on.
+
+	"if element in clean_list" return directly True or False depending on if the 
+	element actually belongs to the list. 
+	The "not" is there to denotes the absence of a specific element, meaning we met it 
+	for the first time. Therefore we append.
+"""
+
+def find_duplicates(your_list):
+
+	clean_list = []
+
+	for element in your_list:
+		if not(element in clean_list):
+			clean_list.append(element)
+
+	return clean_list
+
+if __name__ == "__main__":
+	unittest.main()
