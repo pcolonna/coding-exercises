8576a4a2bab7f4db8faf5ea7d087070f9065e91d
pcolonna <pierre.col.di@gmail.com>
2016-10-29 13:20:20 -0400

    Add 2.2

---
+++ Chapter_2/2.2_kth_element.py
@@ -0,0 +1,11 @@
+# Question 2.1: Return Kth to last.
+
+"""
+	Implement an algorithm to find the kth to last element of a singly linked list.
+
+	We will use deque, double-ended queue. According to the doc, they are implemented 
+	as a doubly-linked list, but will only use operations available to single linked list.
+"""
+
+def find_kth(your_list, k):
+	
