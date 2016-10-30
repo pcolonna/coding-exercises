c76730c8ba9fad2f02a4160da88e5ae4fbd1bcec
pcolonna <pierre.col.di@gmail.com>
2016-10-30 19:23:06 -0400

    Add question 2.3

---
+++ Chapter_2/2.3_delete_mid_node.py
@@ -0,0 +1,7 @@
+# Question 2.3: delete_middle_node.
+
+"""
+	Algo to delete a node in the middle of a singly linked list.
+	Not necessarily the middle, just any node that is not the first or last one.
+"""
+
