4304a3c3ed1088bb362986503b5757b49155635f
pcolonna <pierre.col.di@gmail.com>
2017-05-21 17:20:20 +0200

    First version

--- Chapter_2/2.4_partition.py
+++ Chapter_2/2.4_partition.py
@@ -8,3 +8,28 @@
 	We don't care about ordering inside a partition.
 """
 
+from LinkedList import LinkedList
+
+def partition(list, pivot):
+
+	partitioned_list = LinkedList()
+	node = list.head
+
+	while(node != None):
+		if (node.value < pivot):
+			partitioned_list.add_to_beginning(node.value)
+		else:
+			partitioned_list.add(node.value)
+		node = node.next
+
+	return partitioned_list
+
+if __name__ == "__main__":
+
+	test_list = LinkedList()
+	test_list.add_multiple([3,5,8,5,10,2,1])	#linked list from example.
+
+	print(test_list)
+	print(partition(test_list, 5))
+
+	
