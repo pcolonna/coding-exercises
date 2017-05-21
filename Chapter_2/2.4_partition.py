d683ae3ed8d6aa5a4c06335dc70b0d34a1872f20
pcolonna <pierre.col.di@gmail.com>
2017-05-21 16:39:14 +0200

    question 2.4 initial commit

---
+++ Chapter_2/2.4_partition.py
@@ -0,0 +1,10 @@
+#Question 2.4: Partition
+
+"""
+	Partition a linked list around a value x.
+
+	To do so, we create a new list. 
+	Elements bigger than the pivot are added to the tail, those smaller to the head.
+	We don't care about ordering inside a partition.
+"""
+
