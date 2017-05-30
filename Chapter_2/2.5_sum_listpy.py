6a294f6af2a06a49f762c00e0ced0a55fc4e12fa
pcolonna <pierre.col.di@gmail.com>
2017-05-30 16:42:42 +0200

    Add question 2.5

---
+++ Chapter_2/2.5_sum_listpy.py
@@ -0,0 +1,10 @@
+# Question 2.5 Sum lists
+
+
+from LinkedList import LinkedList
+
+
+# We add the number node by nodes.
+# If necessary, we carry the 1 to the next node.
+# If one list is shorter than the other, we pad the shoter one.
+def sum_list(ll_1, ll_2)
