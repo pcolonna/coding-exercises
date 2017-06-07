6fb25debdffb1e4f83280622bb73ceb7a81d0eb9
pcolonna <pierre.col.di@gmail.com>
2017-06-07 14:33:57 +0200

    Start chap3

---
+++ Chapter_3/3.2_StackMin.py
@@ -0,0 +1,32 @@
+# Question 3.2: Stack Min.
+# 
+#	Design stack which has a function min which returns the min element in O(1)
+
+class StackMin():
+	
+	def __init__(self):
+		self.stack   = []
+		self.minimun = []
+
+	# standard push, which put value on top of stack.
+	# + update minimum if necessary.
+	def push(self, value):
+		
+		self.stack.append(value)								# The push itself.
+
+		if len(sel.minimum) == 0 or value <= self.min[-1]:		# If list empty or if the value is inferior to current min,
+			self.min.append(value)								# append new min. Can have multiple equals minimums.
+
+	# pop the top of stack. Update minimum if necessary.
+	def pop(self):
+		
+		if len(self.stack) == 0:
+			return None											# Empty stack.
+
+		value = self.stack.pop()								# Pop top of the stack. Built-in function.
+
+		if value == self.min[-1]:								# If value equals min, we pop the end of the list. Update mins.							
+			self.min.pop()										# 
+
+		return value
+		
