d1ffa22925f7179679bd5cd03e097902fc80d8ca
pcolonna <pierre.col.di@gmail.com>
2017-06-09 18:34:45 +0200

    Add 3.5 plus clean up

--- Chapter_3/3.4_Queue_via_stacks.py
+++ Chapter_3/3.4_Queue_via_stacks.py
@@ -2,7 +2,7 @@
 
 # Implement a MyQueue class which implements a queue using two stacks
 # But:  Queue -> FIFO (First-in First-out)
-# 		Stack -> LIFO (Last-in First-out)
+#       Stack -> LIFO (Last-in First-out)
 
 # In old_stack, we store elements in the usual stack order. The oldest at the bottom.
 # In new_stack, it's the reverse order. Newest at the top.
@@ -17,50 +17,50 @@
  
 class MyQueue():
 
-	def __init__(self):
-		self.new_stack = []
-		self.old_stack = []
+    def __init__(self):
+        self.new_stack = []
+        self.old_stack = []
 
-	def queue(self,value):
-		self.old_stack.append(value)
+    def queue(self,value):
+        self.old_stack.append(value)
 
-	def dequeue(self):
+    def dequeue(self):
 
-		if (len(self.old_stack) == 0) and (len(self.new_stack) == 0):
-			return None
-		elif len(self.new_stack) == 0:
-			reverse_stack(self.old_stack,self.new_stack)
-		
-		value = self.new_stack.pop()
+        if (len(self.old_stack) == 0) and (len(self.new_stack) == 0):
+            return None
+        elif len(self.new_stack) == 0:
+            reverse_stack(self.old_stack,self.new_stack)
+        
+        value = self.new_stack.pop()
 
-		return value
+        return value
 
 def reverse_stack(stack1, stack2):
-	if len(stack1) == 0:
-		return None
-	while len(stack1) != 0:
-		stack2.append(stack1[-1])
-		stack1.pop()
+    if len(stack1) == 0:
+        return None
+    while len(stack1) != 0:
+        stack2.append(stack1[-1])
+        stack1.pop()
 
 
 if __name__ == '__main__':
-	test_queue = MyQueue()
-	test_queue.dequeue()
+    test_queue = MyQueue()
+    test_queue.dequeue()
 
-	for i in range(10):
-		test_queue.queue(i)
-	for i in range(5):
-		print(test_queue.dequeue())
-	
-	print("")
+    for i in range(10):
+        test_queue.queue(i)
+    for i in range(5):
+        print(test_queue.dequeue())
+    
+    print("")
 
-	for i in range(10,20):
-		test_queue.queue(i)
-	
-	for i in range(5):
-		print(test_queue.dequeue())
+    for i in range(10,20):
+        test_queue.queue(i)
+    
+    for i in range(5):
+        print(test_queue.dequeue())
 
-	print("")
-	for i in range(10):
-		print(test_queue.dequeue())
+    print("")
+    for i in range(10):
+        print(test_queue.dequeue())
 
