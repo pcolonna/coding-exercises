c1456503427422d99a1816e067828fc8caa4ad83
pcolonna <pierre.col.di@gmail.com>
2017-06-09 18:39:25 +0200

    Cleaning up

--- Chapter_3/3.5_Sort_Stack.py
+++ Chapter_3/3.5_Sort_Stack.py
@@ -3,6 +3,8 @@
 # Sort a stack such as the smallest item is on top.
 # Use only one additional temporary stack.
 
+from random import randrange
+
 class Stack(list):
     def peak(self):
         return self[-1]
@@ -11,25 +13,34 @@ class Stack(list):
     def empty(self):
         return len(self) == 0
 
-    def sort_stack(s):
-        r = Stack()
-        while not s.empty():            
-            tmp = s.pop()
-            while not r.empty() and r.peak() > tmp:
-                s.push(r.pop())
-            r.push(tmp)
-            while not s.empty() and s.peak() >= r.peak():
-                #warning, >= here
-                r.push(s.pop())
-        return r
+    def sort_stack(unsorted_stack):
 
-#testing
-from random import randrange
-test_items = [randrange(20) for x in range(20)]
-print(test_items)
-S = Stack()
-for item in test_items:
-    S.push(item)
-S = Stack.sort_stack(S)
-for i, item in enumerate(sorted(test_items)):
-    print("item", item, S[i])
+        sorted_stack = Stack()
+
+        while not unsorted_stack.empty():            
+            tmp = unsorted_stack.pop()
+            
+            while not sorted_stack.empty() and sorted_stack.peak() > tmp:
+                unsorted_stack.push(sorted_stack.pop())
+            sorted_stack.push(tmp)
+            
+            while not unsorted_stack.empty() and unsorted_stack.peak() >= sorted_stack.peak():
+                sorted_stack.push(unsorted_stack.pop())
+        
+        return sorted_stack
+
+
+if __name__ == '__main__':
+
+    
+    test_items = [randrange(20) for x in range(20)]
+    print(test_items)
+    
+    S = Stack()
+    
+    for item in test_items:
+        S.push(item)
+    S = Stack.sort_stack(S)
+    
+    for i, item in enumerate(sorted(test_items)):
+        print("item", item, S[i])
